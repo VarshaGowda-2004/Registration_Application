e3d6bc4149b8444d63602d95bd997ac2e4fdd047
VarshaGowda-2004 <varshahdgowda2004@gmail.com>
2024-12-08 20:48:58 +0530

    sub.php

---
+++ sub.php
@@ -0,0 +1,22 @@
+<?php   echo " 
+<!DOCTYPE html> 
+<html lang='en'> 
+<head> 
+    <meta charset='UTF-8'> 
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'> 
+    <title>Submission Successful</title> 
+    <link rel='stylesheet' href='style.css'> 
+</head> 
+<body> 
+    <div class='form-container'> 
+        <h2>Submission Successful!</h2> 
+        <p><strong>Name:</strong> Varsha H.D</p> 
+        <p><strong>Email:</strong> varshahd2004@gmail.com</p> 
+ 
+        <p><strong>Phone:</strong> 435627890</p> 
+        <p><strong>Date of Birth:</strong> 16-05-2004</p> 
+        <p><strong>Address:</strong> Hassan</p> 
+        <a href='index.html'>Go back to the form</a> 
+    </div> 
+</body> 
+</html> 
